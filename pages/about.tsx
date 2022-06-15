de8ab24234285c1a44d8321963a16cbf30315725
Yanislav Igonin <yanislavdabest@gmail.com>
2022-06-16 00:26:37 +0300

    add about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,20 +3,20 @@ import Head from 'next/head';
 import { Heading, Layout } from '@components';
 import Image from 'next/image';
 
-const Home: NextPage = () => <div>
+const About: NextPage = () => <div>
   <Head>
     <title>Yanislav Igonin</title>
   </Head>
 
   <Layout>
     <div className="mb-4">
-      <Heading text="What's up guys?" />
+      <Heading text="About? How about NO?" />
     </div>
 
     <div className='w-60 h-60 md:w-96 md:h-96 relative animate-spin-slow'>
-      <Image layout='fill' src="/eblo.webp" alt="site owner's face" />
+      <Image layout='fill' src="/ok.png" alt="site owner's face" />
     </div>
   </Layout>
 </div>;
 
-export default Home;
+export default About;
