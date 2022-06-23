bbedfafde36c22cadf2f64555505ca702f64b2e9
Yanislav Igonin <yanislavdabest@gmail.com>
2022-06-23 19:04:55 +0300

    styling

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -8,11 +8,11 @@ interface Props {
   imgSrc: string;
 }
 export const ProjectCard = ({ title, text, imgSrc }: Props) =>
-  <div className="flex flex-col m-2 md:flex-row w-80 shadow-md bg-gray-100 dark:bg-slate-500 rounded-md">
-    <div className="md:w-full">
+  <div className="flex flex-col md:flex-row m-2 w-11/12 md:w-1/2 shadow-md bg-gray-100 dark:bg-slate-500 rounded-md">
+    <div className="w-full md:w-2/6 flex justify-center">
       <Image src={imgSrc} alt={title} width="200px" height="200px" />
     </div>
-    <div className="">
+    <div className="w-full md:w-4/6 p-2 text-center md:text-left">
       <Heading text={title} />
       <Paragraph text={text} />
     </div>
