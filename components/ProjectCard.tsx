0f3b2f311321bfb46bb9c2ee185bebba1265cd0a
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-06 22:59:34 +0300

    car padding

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -7,7 +7,7 @@ type Props = {
   text: string;
 }
 export const ProjectCard = ({ title, text }: Props) =>
-  <div className="flex flex-col md:flex-row m-2 w-11/12 md:w-1/2 shadow-md bg-opacity-80 dark:bg-opacity-80 bg-gray-100 dark:bg-slate-500 rounded-md">
+  <div className="flex flex-col mb-2 p-2 w-11/12 md:w-1/2 shadow-md 80 bg-white dark:bg-slate-500 rounded-md">
     <div className="w-full p-2 text-center md:text-left">
       <div className='text-center'>
         <Heading text={title} />
