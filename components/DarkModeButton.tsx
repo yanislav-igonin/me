2a412beb94e8808dc5ce97382c329b71b8e687f6
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-07 00:30:44 +0300

    darkmode button hover style

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -14,7 +14,7 @@ const darkModePath = <path
   d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
 />;
 const lightColor = 'text-white';
-const darkColor = 'text-slate-600';
+const darkColor = 'text-black';
 
 export const DarkModeButton = () => {
   const [theme, setTheme] = useTheme();
@@ -34,7 +34,7 @@ export const DarkModeButton = () => {
   return <svg
     onClick={() => setTheme(nextTheme)}
     xmlns="http://www.w3.org/2000/svg"
-    className={`cursor-pointer h-10 w-10 ${color}`}
+    className={`cursor-pointer h-10 w-10 ${color} hover:text-rose-500`}
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor"
