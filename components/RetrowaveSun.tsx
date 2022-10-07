34b7c777b9bab5684518c9c19b0d283d5a585871
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-07 11:43:37 +0300

    make sun code compact

--- components/RetrowaveSun.tsx
+++ components/RetrowaveSun.tsx
@@ -1,42 +1,10 @@
 import s from './RetrowaveSun.module.css';
 
+const linesCount = 35;
+const lines = Array.from({ length: linesCount }, (_, i) => i + 1);
+
 export const RetrowaveSun = () => <div className={s.sun}>
   <div className={s.lines}>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
-    <div className={s.line}></div>
+    {lines.map((line) => <div key={line} className={s.line} />)}
   </div>
 </div>;
