d576f415bd4e7c50cbc085855780b56380e76720
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-07 12:31:26 +0300

    landscape darkmode styling

--- components/Layout/RetrowaveSun.tsx
+++ components/Layout/RetrowaveSun.tsx
@@ -6,6 +6,6 @@ const lines = Array.from({ length: linesCount }, (_, i) => i + 1);
 export const RetrowaveSun = () => <div className={s.sun}>
   <div className={s.lines}>
     {lines.map((line) => <div key={line}
-      className={`bg-white dark:bg-orange-400 ${s.line}`} />)}
+      className={`bg-white dark:bg-orange-500 ${s.line}`} />)}
   </div>
 </div>;
