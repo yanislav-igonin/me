da18b0ff17a631c16a15c83d72166234f79673e1
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-17 01:10:40 +0300

    decrease project card scaling on hover

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,7 +9,7 @@ type Props = {
   live: string;
 }
 export const ProjectCard = ({ title, text, github, live }: Props) =>
-  <div className="border border-rose-300 mb-8 p-2 w-11/12 md:w-1/2 bg-white dark:bg-slate-500 rounded-md md:hover:scale-150 duration-100">
+  <div className="border border-rose-300 mb-8 p-2 w-11/12 md:w-1/2 bg-white dark:bg-slate-500 rounded-md md:hover:scale-125 duration-100">
     <div className="w-full p-4">
       <div className='text-center'>
         <Heading text={title} />
