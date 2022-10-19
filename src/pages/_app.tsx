cb7c24a147ad911183c7d70a19cf28cdb292a0a6
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-20 00:44:40 +0300

    add analytics component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,11 @@
 export { reportWebVitals } from 'next-axiom';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Layout } from '@/components';
-import Script from 'next/script';
+import { Analytics, Layout } from '@/components';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
   return <Layout>
-    <Script strategy='lazyOnload'
-      src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
-    <Script id='ga' strategy='lazyOnload' dangerouslySetInnerHTML={{
-      __html: `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${gaId}', {
-        page_path: window.location.pathname,
-      });`
-    }} />
-    
+    <Analytics />
     <Component {...pageProps} />
   </Layout>;
 };
