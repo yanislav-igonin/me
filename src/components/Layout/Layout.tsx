4c24a7147f294817088ddb14717732d33e696615
Yanislav Igonin <yanislavdabest@gmail.com>
2023-01-05 09:26:14 +0200

    add canvas on layout, disable old animations

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,6 +4,7 @@ import s from './Layout.module.css';
 import { RetrowaveLandscape } from './RetrowaveLandscape';
 import Head from 'next/head';
 import { Metatags } from './Metatags';
+import { Canvas } from '../Canvas';
 
 type Props = {
   children: React.ReactNode;
@@ -18,13 +19,15 @@ export const Layout = ({ children }: Props) =>
     <Header />
 
     <main className={'flex h-screen justify-center items-center'}>
-      <div className={`${s.background} fixed -z-10 w-screen h-screen`}>
+      {/* <div className={`${s.background} fixed -z-10 w-screen h-screen`}>
         <RetrowaveLandscape />
         <RetrowaveSun />
       </div>
       <div className='fixed w-screen h-screen top-0 -z-20
         bg-gradient-to-b from-rose-300 to-orange-200
-        dark:bg-gradient-to-b dark:from-purple-900 dark:to-stone-800' />
+        dark:bg-gradient-to-b dark:from-purple-900 dark:to-stone-800' /> */}
+
+      <Canvas className='absolute top-0' draw={(ctx, frameCount) => {}} />
 
       {children}
     </main>
