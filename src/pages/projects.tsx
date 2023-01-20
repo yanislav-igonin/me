6599fb6ac050b78cb3f066af21071c86cb2c436f
Yanislav Igonin <yanislavdabest@gmail.com>
2023-01-21 00:14:08 +0200

    remove micrach from projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -8,11 +8,11 @@ const project = [{
   github: 'https://github.com/yanislav-igonin/ip-crawler-frontend',
   live: 'https://ips.h0b0.dev/',
 }, {
-  title: 'Micrach',
-  text: 'Single board imageboard',
-  github: 'https://github.com/yanislav-igonin/micrach',
-  live: 'https://micrach.igonin.dev/',
-}, {
+//   title: 'Micrach',
+//   text: 'Single board imageboard',
+//   github: 'https://github.com/yanislav-igonin/micrach',
+//   live: 'https://micrach.igonin.dev/',
+// }, {
   title: 'Face Fucker Bot',
   text: 'Telegram bot that uses LQR to fuck up images',
   github: 'https://github.com/yanislav-igonin/face-fucker-bot',
