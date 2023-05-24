399b60ad90854fd40b7bc0f505b7056452a8f72b
Yanislav Igonin <yanislavdabest@gmail.com>
2023-05-24 10:36:30 +0300

    add edges for the box

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,34 +1,47 @@
 // Box.tsx
 import { FC, useEffect, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { Mesh } from 'three';
+import { Mesh, LineSegments, EdgesGeometry, LineBasicMaterial } from 'three';
 
 interface BoxProps {
   position: [number, number, number];
 }
 
 export const Box: FC<BoxProps> = ({ position }) => {
-  const mesh = useRef<Mesh>(null);
+  const boxRef = useRef<Mesh>(null);
+  const edgesRef = useRef<LineSegments>(null);
 
   useFrame(() => {
-    if (mesh.current) {
-      mesh.current.rotation.x += 0.01;
-      mesh.current.rotation.y += .01;
-      mesh.current.rotation.z += .01;
+    if (boxRef.current) {
+      boxRef.current.rotation.x += 0.01;
+      boxRef.current.rotation.y += .01;
+      boxRef.current.rotation.z += .01;
+    }
+    if (edgesRef.current) {
+      edgesRef.current.rotation.x += 0.01;
+      edgesRef.current.rotation.y += .01;
+      edgesRef.current.rotation.z += .01;
     }
   });
 
   // rerender the component when the position changes
   useEffect(() => {
     console.log('rerendering');
-    mesh.current?.position.set(...position);
+    boxRef.current?.position.set(...position);
+    edgesRef.current?.position.set(...position);
   }, [position]);
 
   return (
-    <mesh ref={mesh} position={position}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'orange'} />
-    </mesh>
+    <>
+      <mesh ref={boxRef} position={position}>
+        <boxBufferGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={'orange'} />
+      </mesh>
+      <lineSegments ref={edgesRef}>
+        <edgesGeometry args={[boxRef.current?.geometry]} />
+        <lineBasicMaterial color={'black'} />
+      </lineSegments>
+    </>
   );
 };
 
